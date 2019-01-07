88b7b8adb1f18a1aef123f40f1f2ec57f87c6799
Lana Kostina <exxlain@gmail.com>
2019-01-07 23:09:04 +0700

    styles

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,5 @@
 class CommentsController < ApplicationController
-  http_basic_authenticate_with name: "dhh", password: "secret", only: :destroy
+  http_basic_authenticate_with name: 'dhh', password: 'secret', only: :destroy
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
@@ -12,7 +12,9 @@ class CommentsController < ApplicationController
     @comment.destroy
     redirect_to article_path(@article)
   end
+
   private
+
   def comment_params
     params.require(:comment).permit(:commenter, :body)
   end
